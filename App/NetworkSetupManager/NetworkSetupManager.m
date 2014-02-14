1b62a06315d429e85d62edeca9fdf390d44368fc
Brad Jasper <bjasper@gmail.com>
2014-02-14 00:32:14 -0500

    Fixed bug with AirPort networks

--- App/NetworkSetupManager/NetworkSetupManager.m
+++ App/NetworkSetupManager/NetworkSetupManager.m
@@ -42,12 +42,10 @@
     NSString *output = [NSTask runCommand:command];
     NSString *cleanedOutput = [output stringByTrimmingCharactersInSet:[NSCharacterSet newlineCharacterSet]];
   
-    /*
     // Temporary hack to see if this fixes Focus for people
     if ([cleanedOutput isEqualToString:@"AirPort"]) {
         return @"Wi-Fi";
     }
-     */
     
     return cleanedOutput;
 }
